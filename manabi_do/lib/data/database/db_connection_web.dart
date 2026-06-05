fd286276c444df0a959ee6ffdc97443938ab0b55
mirai-toto <miout-13@hotmail.fr>
2026-06-05 15:04:31 +0200

    Remove initializeDatabase from web connection to fix SQLITE_NOTADB crash

--- manabi_do/lib/data/database/db_connection_web.dart
+++ manabi_do/lib/data/database/db_connection_web.dart
@@ -1,6 +1,5 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
-import 'package:flutter/services.dart';
 
 QueryExecutor openDbConnection() {
   return driftDatabase(
@@ -8,10 +7,6 @@ QueryExecutor openDbConnection() {
     web: DriftWebOptions(
       sqlite3Wasm: Uri.parse('sqlite3.wasm'),
       driftWorker: Uri.parse('drift_worker.js'),
-      initializeDatabase: () async {
-        final blob = await rootBundle.load('assets/manabi_do_content.db');
-        return blob.buffer.asUint8List(blob.offsetInBytes, blob.lengthInBytes);
-      },
     ),
   );
 }
